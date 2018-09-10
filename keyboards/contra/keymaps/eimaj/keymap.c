dda7ca27439eb745cf1ac8709ab8edbc5dce0873
eimaj <jamie@llen.ca>
2018-09-10 15:33:20 -0400

    contra v.1.4

--- keyboards/contra/keymaps/eimaj/keymap.c
+++ keyboards/contra/keymaps/eimaj/keymap.c
@@ -31,7 +31,8 @@ enum planck_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   LOWER,
-  RAISE
+  RAISE,
+  ADJUST
 };
 
 enum {
@@ -49,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Esc  | Tab  | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+   * |Adjust| Tab  | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = {
     {KC_ESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
     {ALL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT) },
-    {KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
 
   /* Lower
@@ -90,45 +91,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = {
     {KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL},
-    {_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV},
+    {_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV},
     {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_LTHAN, KC_GTHAN, KC_BSLS, SFT_T(KC_ENT)},
     {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______,    KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT}
   },
 
   /* Adjust (Lower + Raise)
    * ,-----------------------------------------------------------------------------------.
-   * |      |      |      |      |      |      |      |      |      |      |      |  Del |
-   * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * |      |      |      |      |      |AGnorm|AGswap|Qwerty|Colemk|      |      |      |
-   * |------+------+------+------+------+------|------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+-------------+------+------+------+------+------|
+   * |      |mswhdn|mslfbt|ms-up |msrtbt|      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------|------+------+------+------+------+------|
+   * |      |mswhup|ms-lf |ms-dn |ms-rt |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |             |      |      |      |      |      |
    * `-----------------------------------------------------------------------------------'
    */
   [_ADJUST] = {
-    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
-    {_______, KC_MS_WH_DOWN,   KC_MS_BTN2, KC_MS_UP,   KC_MS_BTN1, AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______},
-    {_______, KC_MS_WH_UP,   KC_MS_LEFT,   KC_MS_DOWN,   KC_MS_RIGHT, _______, _______, _______, _______, _______, _______, _______},
-    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+    {_______, _______,       _______,    _______,    _______,     _______, _______, _______, KC_7, KC_8, KC_9, _______},
+    {_______, KC_MS_WH_DOWN, KC_MS_BTN2, KC_MS_UP,   KC_MS_BTN1,  _______, _______, _______, KC_4, KC_5, KC_6, _______},
+    {_______, KC_MS_WH_UP,   KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______, _______, _______, KC_1, KC_2, KC_3, _______},
+    {_______, _______,       _______,    _______,    _______,     _______, _______, _______, KC_0, KC_DOT, _______, _______}
   }
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
+    // case QWERTY:
+    //   if (record->event.pressed) {
+    //     print("mode just switched to qwerty and this is a huge string\n");
+    //     set_single_persistent_default_layer(_QWERTY);
+    //   }
+    //   return false;
+    //   break;
+    // case COLEMAK:
+    //   if (record->event.pressed) {
+    //     set_single_persistent_default_layer(_COLEMAK);
+    //   }
+    //   return false;
+    //   break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -139,6 +140,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
@@ -146,6 +148,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
       }
       return false;
       break;
@@ -169,7 +180,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case KC_ROCKET:
       if (record->event.pressed) {
-        SEND_STRING("=>"); // this is our macro!
+        SEND_STRING(" => "); // this is our macro!
         return false;
       }
       return false;
