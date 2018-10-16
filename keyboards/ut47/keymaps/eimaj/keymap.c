3b141711e75582ff32f2b97b637d7c20b8894599
eimaj <jamie@llen.ca>
2018-10-16 11:17:34 -0400

    ut47 v 1.2

--- keyboards/ut47/keymaps/eimaj/keymap.c
+++ keyboards/ut47/keymaps/eimaj/keymap.c
@@ -45,12 +45,12 @@ LAYOUT(
   KC_ESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   LCTL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT),
-  KC_HYPR,  KC_LCTL, KC_LALT, KC_LGUI, MO(2),   KC_SPC,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_HYPR,  KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
   /* FN Layer 1
    * ,-------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Delete|
+   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  | Bksp  |
    * |-------------------------------------------------------------------------+
    * |      |     |     |     |     |     |     |  -  |  =  |  [  |  ]  |  \   |
    * |-------------------------------------------------------------------------+
@@ -80,7 +80,7 @@ LAYOUT( /* Right */
    */
 
 LAYOUT( /* Left */
-  KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_BSLS, SFT_T(KC_ENT),
   _______, _______, _______, _______, _______,     KC_MPLY,    _______,    KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT
