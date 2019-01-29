bf98c0395bc60a13d8e82622304fb5ad20fc76be
Jamie Allen <jamie@llen.ca>
2019-01-28 20:54:41 -0500

    Updates to eimaj:contra keymap.c

--- keyboards/contra/keymaps/eimaj/keymap.c
+++ keyboards/contra/keymaps/eimaj/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_QWERTY] = {
     {KC_ESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-    {LCTL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+    {ALL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
     {KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT) },
-    {KC_HYPR,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,  SFT_T(KC_SPC),   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    {KC_LCTL, KC_HYPR, KC_LGUI, KC_LALT, LOWER,  SFT_T(KC_SPC),   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
   },
 
   /* Lower
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
     {_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ROCKET, KC_PIPE, SFT_T(KC_ENT)},
-    {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT}
+    {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END}
   },
 
   /* Raise
