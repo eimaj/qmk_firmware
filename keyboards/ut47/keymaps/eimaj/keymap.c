39c3758b1a1c874d63f86a460dddbfdba29a756b
Jamie Allen <jamie@llen.ca>
2019-01-28 20:54:18 -0500

    Updates to eimaj:ut47 keymap

--- keyboards/ut47/keymaps/eimaj/keymap.c
+++ keyboards/ut47/keymaps/eimaj/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 LAYOUT(
   KC_ESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  LCTL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  ALL_T(KC_TAB), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT),
   KC_HYPR,  KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
@@ -64,7 +64,7 @@ LAYOUT( /* Right */
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_DEL,
   _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, SFT_T(KC_ENT),
-  _______, _______, _______, _______, _______,    KC_MPLY,    _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+  _______, _______, _______, _______, _______,    KC_MPLY,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
   /* FN Layer 2
@@ -83,7 +83,7 @@ LAYOUT( /* Left */
   KC_TILD,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_GRV,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_BSLS, SFT_T(KC_ENT),
-  _______, _______, _______, _______, _______,     KC_MPLY,    _______,    KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT
+  _______, _______, _______, _______, _______,     KC_MPLY,    _______,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
   /* FN Layer 2
