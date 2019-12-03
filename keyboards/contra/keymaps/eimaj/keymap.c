b8bbc7f69f9babd3070bebf54a31918f75e3acf5
Jamie Allen <jamie@llen.ca>
2019-12-03 11:39:39 -0500

    Updates contra keymap with linux switch

--- keyboards/contra/keymaps/eimaj/keymap.c
+++ keyboards/contra/keymaps/eimaj/keymap.c
@@ -21,8 +21,8 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers
 {
-  _QWERTY,
-  _COLEMAK,
+  // _QWERTY,
+  _QWERNUX,
   _LOWER,
   _RAISE,
   _FUNCTION
@@ -31,7 +31,7 @@ enum planck_layers
 enum planck_keycodes
 {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
+  QWERNUX,
   LOWER,
   RAISE,
   FUNCTION
@@ -62,6 +62,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_ENT)},
         {FUNCTION, KC_LCTL, KC_LALT, KC_LGUI, LOWER, SFT_T(KC_SPC), SFT_T(KC_SPC), RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}},
 
+    /* Qwerty Linux
+   * ,-----------------------------------------------------------------------------------.
+   * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+   * |------+------+------+------+------+-------------+------+------+------+------+------|
+   * |Hyp(Tb)|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+   * |------+------+------+------+------+------|------+------+------+------+------+------|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sft(Ent)|
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * | FUNC | Tab  | Alt  | GUI  |Lower | Sft(Space)  |Raise | Left | Down |  Up  |Right |
+   * `-----------------------------------------------------------------------------------'
+   */
+    [_QWERNUX] = {
+        {KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
+        {ALL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
+        {KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_ENT)},
+        {FUNCTION, KC_LGUI, KC_LALT, KC_LCTL, LOWER, SFT_T(KC_SPC), SFT_T(KC_SPC), RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}},
+
     /* Lower
    * ,-----------------------------------------------------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
@@ -73,7 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |     Play    |      | Home | PgDn | PgUp | End  |
    * `-----------------------------------------------------------------------------------'
    */
-    [_LOWER] = {{KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL}, {_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ROCKET, KC_BSLS, SFT_T(KC_ENT)}, {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END}},
+    [_LOWER] = {
+      {KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL}, 
+      {_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_GRV}, 
+      {_______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ROCKET, KC_BSLS, SFT_T(KC_ENT)}, 
+      {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END}},
 
     /* Raise
    * ,-----------------------------------------------------------------------------------.
@@ -86,7 +107,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |     Play    |      | Prev | Vol- | Vol+ | Next |
    * `-----------------------------------------------------------------------------------'
    */
-    [_RAISE] = {{KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC}, {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_GRV}, {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, SFT_T(KC_ENT)}, {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT}},
+    [_RAISE] = {
+      {KC_TILD, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC}, 
+      {_______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_GRV}, 
+      {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PIPE, SFT_T(KC_ENT)}, 
+      {_______, _______, _______, _______, _______, KC_MPLY, KC_MPLY, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT}},
 
     /* Function
    * ,-----------------------------------------------------------------------------------.
@@ -99,7 +124,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |             |      |  0   |  .   |      |      |
    * `-----------------------------------------------------------------------------------'
    */
-    [_FUNCTION] = {{_______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, KC_BSPC}, {KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, _______, _______, KC_4, KC_5, KC_6, _______}, {KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_1, KC_2, KC_3, _______}, {_______, _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, _______, _______}}};
+    [_FUNCTION] = {
+      {_______, QWERTY, QWERNUX, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, KC_BSPC}, 
+      {KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, _______, _______, KC_4, KC_5, KC_6, _______}, 
+      {KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_1, KC_2, KC_3, _______}, 
+      {_______, _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, _______, _______}}};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
